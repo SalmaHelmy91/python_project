7201f8a00a58019d283b6cf3557becf8abe335b6
SalmaMahmoud91 <47832791+SalmaMahmoud91@users.noreply.github.com>
2021-02-20 05:34:36 +0200

    hello_world

--- first.py
+++ first.py
@@ -1,2 +1,13 @@
-# Hello world program #
+## Hello world program ##
+#print ("Hello there!")
+#name = input ("What is your name? ")
+#print ("nice to meet you", name, "!")
+
+## Making a function ##
+#def main():
+#    print ("Hello there!")
+#    name = input ("What is your name? ")
+#    print ("nice to meet you", name, "!")
+#if __name__ == "__main__" :
+#    main()
 
